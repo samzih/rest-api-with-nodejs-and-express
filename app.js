a782ed830d44713f802f239ebde85f46251d04e6
Sam Azimi <samzih1337@gmail.com>
2023-01-03 16:52:50 +0100

    GET endpoint for getting all the objects

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require('express');
+const express = require('express'); // Includes the Express framework
 const app = express();
-const port = 3000;
+const port = 3000; // Sets the port to 3000 (standard for express)
+const fs = require('fs'); // Includes the Node.js File System Module
 
+// GET endpoint
+app.get('/api/players', (req, res) => {
+    fs.readFile('./players.json', (err, data) => {
+        res.status(200).send(JSON.parse(data));
+    });
+  });
 
-
-app.listen(port, () => console.log('Server is up and running!'));
+app.listen(port, () => console.log(`Server is up and running on port: ${port}`));
